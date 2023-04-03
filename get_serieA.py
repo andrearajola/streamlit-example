9c9dbfb17a18ebdd19e80c7af180494d4ec7a76d
andrearajola <102627835+andrearajola@users.noreply.github.com>
2023-04-03 11:45:57 +0200

    Update get_serieA.py

--- get_serieA.py
+++ get_serieA.py
@@ -1,12 +1,23 @@
 # streamlit_app.py
 
 import streamlit as st
-import snowflake.connector
+from snowflake.snowpark.session import Session
 
-my_db.connect(username=st.secrets.DB_USERNAME, password=st.secrets.DB_PASSWORD, account="xb47661.eu-west-2.aws",role="sysadmin",)
+def create_session_object():
+   connection_parameters = {
+      "account": "xb47661.eu-west-2.aws",
+      "user": "ARAJOLA",
+      "password": st.secrets.DB_PASSWORD,
+      "role": "SYSADMIN",
+      "warehouse": "COMPUTE_WH",
+      "database": "DEMO",
+      "schema": "STREAMLIT"
+   }
+   session = Session.builder.configs(connection_parameters).create()
+   return session
 
 def run_query(query):
-    with conn.cursor() as cur:
+    with session.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
