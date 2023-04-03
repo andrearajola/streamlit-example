330daa68593e6026dd0aa09eaf00ea9f50f4b75c
andrearajola <102627835+andrearajola@users.noreply.github.com>
2023-04-03 11:41:57 +0200

    Update get_serieA.py

--- get_serieA.py
+++ get_serieA.py
@@ -1,2 +1,17 @@
-import streamlit as st
+# streamlit_app.py
 
+import streamlit as st
+import snowflake.connector
+
+my_db.connect(username=st.secrets.DB_USERNAME, password=st.secrets.DB_PASSWORD, account="xb47661.eu-west-2.aws",role="sysadmin",)
+
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from DEMO.STREAMLIT.SERIE_A_COMPLETE;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
